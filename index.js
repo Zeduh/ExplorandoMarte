5ec7ec35f06d8fc49e5c21ff32d474ea4b55653a
root <root@PCdoZeduh.localdomain>
2022-08-24 15:51:30 -0300

    atualizado a estrutura geral do código e comentarios

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const {readFileSync, promises: fsPromises} = require('fs')
-
 /* Inicializadores */
+const {readFileSync, promises: fsPromises} = require('fs')
 let tamanhoPlanalto = {minX: 0, minY: 0, maxX: 0, maxY: 0}
 let listaSondas = []
 const direcoesValidas = ['N', 'E', 'S', 'W'];
 const comandosMovimentoValidos = ['L', 'R', 'M', 'P'];
 
-// Leitura e tratamento dos dados
+/*Leitura, tratamento e iteração dos dados. */
 // Receber conteudo do arquivo
 function pegarDados() {
     const encoding = 'utf-8';
@@ -19,16 +18,22 @@ function converterConteudoLinha() {
     let arrayConteudo = pegarDados().split('\n').map(linha => linha.trim());
     return arrayConteudo
 }
-
-/* Utils */
-// Verifica se a variável é um numero
-function isNumber (valor) {
-    return !isNaN(Number(valor)) && isFinite(valor);
-}
-
-//Retorna a saida com as posições x e y das sonda junto com a direção
-function saidaSondas () {
-    listaSondas.forEach((sonda) => console.log(`${sonda.x} ${sonda.y} ${sonda.direcao}`))
+// Iteracao para definir tamanho da area, sondas e suas instruções
+function iterarLinhas() {
+    let linhas = converterConteudoLinha()
+    linhas.forEach((comandos, indexLinha) => {
+        if(indexLinha === 0) {
+            definirCoordenadaMaxima(comandos);
+        } else if(indexLinha % 2 === 1) {
+            // linhas pares devem ser coordenadas de posição inicial de uma sonda.
+            criarNovaSonda(comandos);
+        } else {
+            // linhas impares devem ser instruções de movimentação de uma sonda.
+            realizarInstrucoesSonda(comandos);
+        }
+    });
+    
+    saidaSondas();
 }
 
 /* Manipuladores */
@@ -62,6 +67,8 @@ function criarNovaSonda(comandos) {
     }
 }
 
+// Recebe comandos e realiza instruções 
+// Envia erro em caso de dados inválidos
 function realizarInstrucoesSonda(comandos) {
     const listaComandos = comandos.split('');
     const indexSondaAtual = listaSondas.length - 1;
@@ -70,12 +77,12 @@ function realizarInstrucoesSonda(comandos) {
             switch(comando) {
                 case 'L':
                 case 'R':
-                    console.log('Virando Sonda')
-                    // virarSonda(comando, indexSondaAtual)
+                    // console.log('Virando Sonda') /* TESTE */
+                    virarSonda(comando, indexSondaAtual)
                     break;
                 case 'M':
-                    console.log('Movendo Sonda')
                     //moverSonda(indexSondaAtual)
+                    // console.log('Movendo Sonda') TESTE
                     break;
                 case 'P':
                 default:
@@ -87,24 +94,39 @@ function realizarInstrucoesSonda(comandos) {
     })
 }
 
-function iterarLinhas() {
-    let linhas = converterConteudoLinha()
-    linhas.forEach((comandos, indexLinha) => {
-        if(indexLinha === 0) {
-            definirCoordenadaMaxima(comandos);
-        } else if(indexLinha % 2 === 1) {
-            // linhas pares devem ser coordenadas de posição inicial de uma sonda.
-            criarNovaSonda(comandos);
-        } else {
-            // linhas impares devem ser instruções de movimentação de uma sonda.
-            realizarInstrucoesSonda(comandos);
+// Realiza ação de virar a sonda 
+// Envia erro em caso de sonda não encontrada
+function virarSonda(direcao, indexSonda) {
+    const sonda = listaSondas[indexSonda];
+    const virarDirecao = direcao === 'L' ? -1: 1;
+    if(sonda) {
+        const indexDirecaoSondaAtual = direcoesValidas.findIndex((direcao) => direcao === sonda.direcao);
+
+        let indexNovaDirecao = indexDirecaoSondaAtual + virarDirecao;
+        if(indexNovaDirecao < 0) {
+            indexNovaDirecao = direcoesValidas.length-1;
+        } else if(indexNovaDirecao === direcoesValidas.length){
+            indexNovaDirecao = 0;
         }
-    });
-    
-    saidaSondas();
+        listaSondas[indexSonda].direcao = direcoesValidas[indexNovaDirecao];
+        // console.log(`Virando sonda para ${sonda.direcao}`) /* TESTE */
+        // console.log(listaSondas) /* TESTE */
+    } else {
+        throw new Error('Sonda não encontrada');
+    }
 }
 
+/* Utils */
+// Verifica se a variável é um numero
+function isNumber (valor) {
+    return !isNaN(Number(valor)) && isFinite(valor);
+}
 
-console.log(iterarLinhas())
+//Retorna a saida com as posições x e y das sonda junto com a direção
+function saidaSondas () {
+    listaSondas.forEach((sonda) => console.log(`${sonda.x} ${sonda.y} ${sonda.direcao}`))
+}
+
+iterarLinhas()
 // console.log(tamanhoPlanalto)
 // console.log(listaSondas)
