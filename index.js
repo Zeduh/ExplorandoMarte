723f26489a55b72d2f0ec53c36bb69813c5fd9e5
root <root@PCdoZeduh.localdomain>
2022-08-24 12:00:33 -0300

    implementado função para converter linhas do conteudo em um array de linhas

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const fs = require('fs')
+const {readFileSync, promises: fsPromises} = require('fs')
 
-
+/* Receber conteudo do arquivo */
 function pegarDados() {
     const encoding = 'utf-8';
-    const path = './dados/entradaDados.txt';
-    let entradaDados = fs.readFile(path, encoding, (erro, entradaDados) =>{
-        if(erro) {
-            console.log(erro)
-        }
-        console.log(entradaDados);
-    });
+    const caminho = './dados/entradaDados.txt';
+    const conteudo = readFileSync(caminho, encoding);
+    return conteudo
 }
 
-pegarDados()
+function converterConteudoLinha() {
+    let linhas = pegarDados().split('\n').map(linha => linha.trim());
+    return linhas
+}
+
+console.log(converterConteudoLinha())
